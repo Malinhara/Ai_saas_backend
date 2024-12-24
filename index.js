2315ab1f2bd4abff89f29f8ca4d544ad7fba1707
Malinhara <poornamalinhara53@gmail.com>
2024-12-24 17:09:07 +0530

    second

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ connectDB();
 // Middleware to serve static files
 // Middleware
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://tubular-nougat-1d6124.netlify.app',
     credentials: true
   }));
   app.use(express.json());
