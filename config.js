e292e48120f724a31825bc7cdca844e471a77a25
Poorna Malinhara Kuruppu Arachchi <131478375+Malinhara@users.noreply.github.com>
2024-12-26 21:03:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const config = {
   EMAIL: process.env.EMAIL_USER || '', // Default to an empty string if not set
   PASSWORD: process.env.EMAIL_PASS || '', // Default to an empty string if not set
-  PASSWORD: process.env.EMAIL_PASS || '', // Default to an empty string if not set
   OPENAI_API_KEY: process.env.OPENAI_API_KEY || '', // Default to an empty string if not set
   DDI: process.env.DDI || '', // Default to an empty string if not set
 
