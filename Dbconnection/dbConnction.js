cbc764e8aa63253d125015c46983e6995f2886b5
Poorna Malinhara Kuruppu Arachchi <131478375+Malinhara@users.noreply.github.com>
2024-12-26 21:17:21 +0530

    Update dbConnction.js

--- Dbconnection/dbConnction.js
+++ Dbconnection/dbConnction.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const connectDB = async () => {
-  try {
-    const uri = "mongodb+srv://poornainvest0:kkswVuGWJvCJechn@aiapp.iog0q.mongodb.net/?retryWrites=true&w=majority&appName=Aiapp";
-    await mongoose.connect(uri);
-    console.log('Connected to MongoDB Atlas');
-  } catch (err) {
-    console.error('Error connecting to MongoDB Atlas:', err);
-    process.exit(1); // Exit the process with a failure code
-  }
-};
+// const connectDB = async () => {
+//   try {
+//     const uri = "mongodb+srv://poornainvest0:kkswVuGWJvCJechn@aiapp.iog0q.mongodb.net/?retryWrites=true&w=majority&appName=Aiapp";
+//     await mongoose.connect(uri);
+//     console.log('Connected to MongoDB Atlas');
+//   } catch (err) {
+//     console.error('Error connecting to MongoDB Atlas:', err);
+//     process.exit(1); // Exit the process with a failure code
+//   }
+// };
 
-module.exports = connectDB;
+// module.exports = connectDB;
