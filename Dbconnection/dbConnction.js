94c6d5fcf37ce25a9ff1cea427016b9d587a7678
Poorna Malinhara Kuruppu Arachchi <131478375+Malinhara@users.noreply.github.com>
2024-12-26 20:37:52 +0530

    Update dbConnction.js

--- Dbconnection/dbConnction.js
+++ Dbconnection/dbConnction.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const uri = process.env.MONGO_URI;
+    const uri = "mongodb+srv://poornainvest0:kkswVuGWJvCJechn@aiapp.iog0q.mongodb.net/?retryWrites=true&w=majority&appName=Aiapp";
     if (!uri) throw new Error('MongoDB URI is not defined');
     await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log('MongoDB connected successfully');
