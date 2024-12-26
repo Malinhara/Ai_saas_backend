f38c943c95dad7023a5650525b16ab9a1b968ddd
Poorna Malinhara Kuruppu Arachchi <131478375+Malinhara@users.noreply.github.com>
2024-12-26 20:10:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 
 const config = {
-
-  GPT_KEY: process.env.GPT_API_KEY || '' 
-
+  EMAIL: process.env.EMAIL_USER || '', // Default to an empty string if not set
+  PASSWORD: process.env.EMAIL_PASS || '', // Default to an empty string if not set
+  PASSWORD: process.env.EMAIL_PASS || '', // Default to an empty string if not set
+  GPT_KEY: process.env.GPT_API_KEY || '', // Default to an empty string if not set
+  DDI: process.env.DDI || '', // Default to an empty string if not set
 
 };
 
