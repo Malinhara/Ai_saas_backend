20d11a821f8141b236ce4523e4cfcd862fdcfd6d
Poorna Malinhara Kuruppu Arachchi <131478375+Malinhara@users.noreply.github.com>
2024-12-26 20:40:11 +0530

    Update dbConnction.js

--- Dbconnection/dbConnction.js
+++ Dbconnection/dbConnction.js
@@ -3,12 +3,11 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     const uri = "mongodb+srv://poornainvest0:kkswVuGWJvCJechn@aiapp.iog0q.mongodb.net/?retryWrites=true&w=majority&appName=Aiapp";
-    if (!uri) throw new Error('MongoDB URI is not defined');
-    await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    console.log('MongoDB connected successfully');
-  } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
-    process.exit(1);
+    await mongoose.connect(uri);
+    console.log('Connected to MongoDB Atlas');
+  } catch (err) {
+    console.error('Error connecting to MongoDB Atlas:', err);
+    process.exit(1); // Exit the process with a failure code
   }
 };
 
