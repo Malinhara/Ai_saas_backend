2d3860617a5c6890be90b5aade369d47aa12781d
Poorna Malinhara Kuruppu Arachchi <131478375+Malinhara@users.noreply.github.com>
2024-12-31 20:15:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ const config = {
   EMAIL: process.env.EMAIL_USER || '', // Default to an empty string if not set
   PASSWORD: process.env.EMAIL_PASS || '', // Default to an empty string if not set
   OPENAI_API_KEY: process.env.OPENAI_API_KEY || '', // Default to an empty string if not set
+  GPT_CHAT_KEY: process.env.OPENAI_CHAT_API_KEY || '', 
   DDI: process.env.DDI || '', // Default to an empty string if not set
 
 };
