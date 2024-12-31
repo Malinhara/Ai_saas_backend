a2077fd5ea9cd31dbc14596588506787211b0bf9
Poorna Malinhara Kuruppu Arachchi <131478375+Malinhara@users.noreply.github.com>
2024-12-31 20:20:10 +0530

    Update user.js

--- Route/user.js
+++ Route/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const UserService = require('../Services/user');
+const chatService = require('../Services/chat')
 const Auth = require('../Auth/auth');
 
 
@@ -40,6 +41,32 @@ userRouter.post('/google-register', async (req, res) => {
     const response = await UserService.userUpdate(req.body);
     res.status(response.statusCode).json(response);
   });
+
+
+
+  userRouter.post('/twitter', async (req, res) => {
+    const response = await UserService.userTwitter(req.body);
+    res.status(response.statusCode).json(response);
+  });
+
+  userRouter.post('/twitter/post', async (req, res) => {
+    
+    const response = await UserService.userTwitterPost(req.body);
+    res.status(response.statusCode).json(response);
+  });
+  
+
+  userRouter.post('/chat/personality', async (req, res) => {
+    const response = await chatService.chatPersonality(req.body);
+    res.status(response.statusCode).json(response);
+  });
+
+  userRouter.post('/chat', async (req, res) => {
+    const response = await chatService.chat(req.body);
+    res.status(response.statusCode).json(response);
+  });
+
+
   
 
   userRouter.get('/verify', Auth.authenticate, (req, res) => {
