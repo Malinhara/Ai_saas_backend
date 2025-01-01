af16ebf8042ff737f0a56f6e27c2720b80c8dcc8
Poorna Malinhara Kuruppu Arachchi <131478375+Malinhara@users.noreply.github.com>
2025-01-01 12:37:57 +0530

    Update user.js

--- model/user.js
+++ model/user.js
@@ -3,9 +3,14 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true},
-  salt: { type: String, required: true }, 
+  salt: { type: String, required: true },
   code:{type:String,require:false},
-  status: { type: String,required: true  }, 
+  status: { type: String,required: true  },
+  appKey: { type: String, required: false },
+  appSecret: { type: String, required: false },
+  accessToken: { type: String, required: false },
+  accessSecret: { type: String, required: false },
+  personality:{type:String, require:false}
  
 });
 
